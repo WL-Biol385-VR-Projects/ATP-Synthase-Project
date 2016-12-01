e0ee2f581daffd334208df89a94ebcbdb984b9cf
jbowman010 <jbowman010@gmail.com>
2016-12-01 15:29:09 -0500

    Add in interactables and door

---
+++ Unity/New Unity Project/Assets/TriggerChangeScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TriggerChangeScene : MonoBehaviour
+{
+
+    public string scene;
+
+    void OnTriggerEnter()
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
