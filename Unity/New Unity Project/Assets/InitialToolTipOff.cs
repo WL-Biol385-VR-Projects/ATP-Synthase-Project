1aea92cf10704e358ab9fd100d5fe31473d7a8a1
jbowman010 <jbowman010@gmail.com>
2016-12-04 17:47:26 -0500

    Add in controller tooltops

---
+++ Unity/New Unity Project/Assets/InitialToolTipOff.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitialToolTipOff : MonoBehaviour
+{
+
+    //place this script onto the controller
+
+    //drag the controller here from the hierarchy
+
+    public SteamVR_TrackedObject controller;
+
+    //drag the Tool Tips GameObject here from the hierarchy
+
+    public GameObject tips;
+
+    void Update()
+    {
+        var device = SteamVR_Controller.Input((int)controller.index);
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
+        {
+            tips.SetActive(false);
+        }
+    }
+}
---
+++ Unity/New Unity Project/Assets/MenuToolTips.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuToolTips : MonoBehaviour
+{
+
+    //place this script onto the controller
+
+    //drag the controller here from the hierarchy
+
+    public SteamVR_TrackedObject controller;
+
+    //drag the Tool Tips GameObject here from the hierarchy
+    
+    public GameObject tips;
+
+    //this sets the default state of your on/off switch
+
+    private bool onoff = false;
+
+    //this will turn tool tips off and on with the Application Meun button
+
+    void Update()
+    {
+        var device = SteamVR_Controller.Input((int)controller.index);
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
+        {
+            onoff = !onoff;
+
+            if (onoff)
+            {
+                tips.SetActive(true);
+            }
+            else
+            {
+                tips.SetActive(false);
+            }
+        }
+    }
+}
