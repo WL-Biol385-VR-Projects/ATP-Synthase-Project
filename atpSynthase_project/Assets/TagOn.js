b05724803552504a051aca5d81627e7905eea669
bowmanj17 <bowmanj17@mail.wlu.edu>
2017-03-03 08:42:33 -0500

    Fix tag snaps and add in preliminary audio triggering

--- atpSynthase_project/Assets/TagOn.js
+++ atpSynthase_project/Assets/TagOn.js
@@ -1,10 +1,16 @@
 ﻿#pragma strict
 
-var thing : GameObject;
+var inactiveThing1 : GameObject;
+var activeThing1 : GameObject;
+var activeThing2 : GameObject;
+var activeThing3 : GameObject;
 var objectTag: String;
 
 function OnTriggerEnter (other : Collider) {
     if(other.gameObject.tag == objectTag){
-        thing.SetActive(true);
+        inactiveThing1.SetActive(true);
+        activeThing1.SetActive(false);
+        activeThing2.SetActive(false);
+        activeThing3.SetActive(false);
     }
 }
---
+++ atpSynthase_project/Assets/VRTK/Prefabs/Resources/Scripts/playSound.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var Sound : AudioClip;
+
+private var hasPlayed = false;
+
+function OnTriggerEnter()
+{
+    if (!hasPlayed)
+    {
+        GetComponent.<AudioSource>().PlayOneShot(Sound);
+        hasPlayed = true;
+        Destroy(gameObject);
+    }
+}
