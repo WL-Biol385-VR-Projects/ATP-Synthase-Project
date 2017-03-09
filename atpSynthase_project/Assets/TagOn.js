3667eb6125ab605ac9ed9d5a3dddb0c1c61e1749
bowmanj17 <bowmanj17@mail.wlu.edu>
2017-03-09 11:36:28 -0500

    Add in audio triggers
    Need to change triggers to when controller enters subunit initially (to
    match script) (possibly put unrendered game object as trigger with tag
    on controller?). Script also needs to be edited t be pronounced F oh,
    and to direct player to next scene through end door.

--- atpSynthase_project/Assets/TagOn.js
+++ atpSynthase_project/Assets/TagOn.js
@@ -1,6 +1,7 @@
 ﻿#pragma strict
 
 var inactiveThing1 : GameObject;
+var inactiveThing2 : GameObject;
 var activeThing1 : GameObject;
 var activeThing2 : GameObject;
 var activeThing3 : GameObject;
@@ -9,6 +10,7 @@ var objectTag: String;
 function OnTriggerEnter (other : Collider) {
     if(other.gameObject.tag == objectTag){
         inactiveThing1.SetActive(true);
+        inactiveThing2.SetActive(true);
         activeThing1.SetActive(false);
         activeThing2.SetActive(false);
         activeThing3.SetActive(false);
